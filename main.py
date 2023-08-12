4f119a1faeea7e39eda75b7f33631eded0a9b7e8
JAAR <joe.aaron.acosta@outlook.com>
2023-08-12 13:06:29 -0400

    Version 0

---
+++ main.py
@@ -0,0 +1,16 @@
+'''
+>>> JAAR
+>>> 08/12/2023
+>>> Practicing Fundamentals Program 18
+>>> Version 0
+'''
+
+'''
+>>> Generates a program that prints a story to the console then asks the user if they would like to search through the story for all occurrences of a word and/or if they would like to replace a single word in the story for another.
+'''
+
+def main() :
+    pass
+
+if __name__ == '__main__' :
+    main()
